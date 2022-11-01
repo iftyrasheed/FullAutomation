645cf0d8f53998d0fe586966ba197f4999ae4edd
Aminur Rasheed Ifty <alienifty@gmail.com>
2022-11-01 08:34:18 +0600

    third commit

--- src/test/java/com/saucedemo/FirstTest.java
+++ src/test/java/com/saucedemo/FirstTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -14,15 +15,20 @@ public class FirstTest {
 
     WebDriver driver;
 
-    @Test(priority = 0)
+    @Test(priority = 0 , enabled = true)
     void startTesting(){
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
         driver.get("https://www.saucedemo.com/");
 
+        String expectedTitle = "Swag Labs";
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals(actualTitle,expectedTitle);
+
     }
 
-    @Test(priority = 1)
+    @Test(priority = 1, enabled = true)
     void runTesting() throws InterruptedException {
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
@@ -32,7 +38,7 @@ public class FirstTest {
 
     }
 
-    @Test(priority = 2)
+    @Test(priority = 2, enabled = true)
     void endTesting(){
         driver.quit();
     }
